a6caec26044f9ee518d0660c6ddd6ec37570af12
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-18 16:48:24 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict";
+
+/**
+ * This function calculates the movie rating
+ */
+function myButtonClicked() {
+  const userAge = document.getElementById("age").value;
+  document.getElementById("age").innerHTML = userAge;
+
+  if (userAge >= 17) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see any movie.";
+  } else if (userAge >= 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see a PG 13 rated movie.";
+  } else if (userAge >= 5) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a PG or G rated movie.";
+  } else {
+    document.getElementById("movie-rating").innerHTML =
+      "You are too young for most movies.";
+  }
+}
