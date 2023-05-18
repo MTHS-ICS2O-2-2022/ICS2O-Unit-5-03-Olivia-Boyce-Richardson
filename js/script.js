b9a94a8ca0981c3121b88440248f6950ea34e14e
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-18 17:02:25 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Olivia B-R All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia B-R
+// Created on: may 2023
 // This file contains the JS functions for index.html
 
 "use strict";
